ba2cace4571dce61fdfcbce048b101d51f64ff58
falletta15 <af104@zips.uakron.edu>
2022-06-13 09:51:57 -0400

    Start of Pathfinding Testing
    Cloning Sebastian League Astar Pathfinding for use.

--- DantaeVer0/Assets/InputMaps/PlayerInput.cs
+++ DantaeVer0/Assets/InputMaps/PlayerInput.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/PlayerInput.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/InputMaps/PlayerInput.inputactions'
 
 using System;
 using System.Collections;
--- DantaeVer0/Assets/InputMaps/ProjectileInput.cs
+++ DantaeVer0/Assets/InputMaps/ProjectileInput.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/ProjectileInput.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/InputMaps/ProjectileInput.inputactions'
 
 using System;
 using System.Collections;
