970b18359334617da0dbbfc7c366bc64066f8a83
falletta15 <af104@zips.uakron.edu>
2022-06-21 09:22:10 -0400

    Correct A* Pathfinding Up to E07 + New Request
    Have pathfinding working properly up to E07 of Sebastian League and implement path reset for new requests.

--- DantaeVer0/Assets/Scripts/Pathfinding/Unit.cs
+++ DantaeVer0/Assets/Scripts/Pathfinding/Unit.cs
@@ -1,63 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.InputSystem;
 
 public class Unit : MonoBehaviour
 {
-    //! Bugs on recalculating moves
-   public Transform target;
-   float speed = 20.0f;
-   //float rotationSpeed = 5.0f;
 
-   Vector3[] path;
-   int targetIndex;
 
-   private void Start() {
-    PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
-   }
-  
-   private void Update() {
+	public Transform target;
+	float speed = 20;
+	Vector3[] path;
+	int targetIndex;
+
+	void Start() {
+		PathRequestManager.RequestPath(transform.position,target.position, OnPathFound);
+	}
+
+	 private void Update() {
     //! Using Update to Recalculate if Target Moves
+	//! Unit does not move exactly to player
     //*Need to have path updated during Update and not create queque so instead when change to path happens it is performed
     //*Update of path in conjuction with path update might be best
-    //PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+    
         if (Keyboard.current[Key.Space].wasPressedThisFrame)
         {
             PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
         }
    }
-   
-        
-   
 
-   public void OnPathFound(Vector3[] newPath, bool pathSuccessful){
-    if (pathSuccessful){
-        path = newPath;
-        targetIndex = 0;
-        StopCoroutine("FollowPath");
-        StartCoroutine("FollowPath");
-    }
-   }
+	public void OnPathFound(Vector3[] newPath, bool pathSuccessful) {
+		if (pathSuccessful) {
+			path = newPath;
+			//targetIndex = 0;
+			StopCoroutine("FollowPath");
+			StartCoroutine("FollowPath");
+		}
+	}
 
-   IEnumerator FollowPath(){
+	IEnumerator FollowPath() {
+		Vector3 currentWaypoint = path[0];
 
-        Vector3 currentWaypoint = path[0];
+		while (true) {
+			if (transform.position == currentWaypoint) {
+				targetIndex ++;
+				if (targetIndex >= path.Length) {
+					targetIndex = 0;
+					path = new Vector3[0];
+					yield break;
+				}
+				currentWaypoint = path[targetIndex];
+			}
 
-        while(true)
-        {
-            if (transform.position == currentWaypoint){
-                targetIndex++;
-                if (targetIndex >= path.Length){
-                    //targetIndex = 0;
-                    //path = new Vector3[0];
-                    yield break;
-                }
-                currentWaypoint = path[targetIndex];
-            }
-
-            /*
-            //! Fix having right or x local axis move towards target
+			//Unit Direction Towards Waypoints
+			/*
+			//! Fix having right or x local axis move towards target
             //Rotate Towards Waypoint
             Debug.Log(currentWaypoint + " waypoint position");
             Vector3 targetDir = currentWaypoint - transform.position;
@@ -68,33 +63,26 @@ public class Unit : MonoBehaviour
             transform.rotation = Quaternion.LookRotation(newDir,-Vector3.forward);
             */
 
-            //Move Towards Waypoint
-            transform.position = Vector3.MoveTowards(transform.position,currentWaypoint,speed*Time.deltaTime);
-            yield return null;
-        }
-    }
+			//Unit Movement Towards Waypoints
+			transform.position = Vector3.MoveTowards(transform.position,currentWaypoint,speed * Time.deltaTime);
+			yield return null;
 
-//? Line not always rendered will need to refactor
-    public void OnDrawGizmos(){
-        if (path != null){
-            Gizmos.color = Color.black;
-            Gizmos.DrawCube(transform.position, Vector3.one);
+		}
+	}
 
-            for (int i=targetIndex; i<path.Length; i++){
-                Gizmos.color = Color.black;
-                Gizmos.DrawCube(path[i], Vector3.one);
-                
-                if (i == targetIndex){
-                    Gizmos.DrawLine(transform.position,path[i]);
-                }
-                else{
-                    //*Refactor player line to first path
-                    
-                    Gizmos.DrawLine(path[i-1],path[i]);
-                }
-            }
-        }
-    }
-    
+	public void OnDrawGizmos() {
+		if (path != null) {
+			for (int i = targetIndex; i < path.Length; i ++) {
+				Gizmos.color = Color.black;
+				Gizmos.DrawCube(path[i], Vector3.one);
 
+				if (i == targetIndex) {
+					Gizmos.DrawLine(transform.position, path[i]);
+				}
+				else {
+					Gizmos.DrawLine(path[i-1],path[i]);
+				}
+			}
+		}
+	}
 }
